edebbd839ef5f39a48a637026236353bea74db37
kevinreutter <kevin.reutter@icloud.com>
2016-11-14 16:34:51 +0100

    Erste vollständig Funktionierende Version

--- 01-Triangle/triangle.js
+++ 01-Triangle/triangle.js
@@ -1,16 +1,24 @@
+var canvas;
 var gl;
 var num_vertex;
 
 var current_positionX = 0.0;
 var current_positionY = 0.0;
-var current_rotation = 40;
+var current_rotation = 0;
+var radius;
+var program;
 
 window.onload = function init()
 {
-	drawPacman(0.2, 35, 50);
+	canvas =  document.getElementById("gl-canvas");
+	gl = WebGLUtils.setupWebGL(canvas);
+	program = initShaders(gl, "vertex-shader", "fragment-shader");
+
+
+
+	radius = 0.05;
+	drawPacman(radius, 35, 50);
 	render();
-
-
 
 	requestAnimFrame(drawAndRender);
 };
@@ -18,7 +26,7 @@ window.onload = function init()
 
 var drawAndRender = function()
 {
-	drawPacman(0.2, 35, 50);
+	rotateAndTranslate();
 	render();
 	requestAnimFrame(drawAndRender);
 };
@@ -29,31 +37,19 @@ function degreesToRadians(x)
 	return x * (Math.PI / 180);
 }
 
-var canvas;
-var gl;
 
 function drawPacman(radius, numberOfVertices, angleMouth)
 {
-	// Get canvas and setup WebGL
-	canvas =  document.getElementById("gl-canvas");
-	//var canvas = document.getElementById("gl-canvas");
-	gl = WebGLUtils.setupWebGL(canvas);
-	if (!gl) { alert("WebGL isn't available"); }
-
-
-	// KOORDINATEN BERECHNEN
-
 	var koordinaten = [];
 	var colors = [];
 	var winkel = 360 / numberOfVertices;
 
 	koordinaten.push(0);
 	koordinaten.push(0);
-	colors.push(1);
+	colors.push(0.023);
+	colors.push(0.572);
+	colors.push(0.615);
 	colors.push(1.0);
-	colors.push(1);
-	colors.push(1.0);
-
 
 	num_vertex = 1;
 
@@ -67,9 +63,9 @@ function drawPacman(radius, numberOfVertices, angleMouth)
 			koordinaten.push(Math.cos(degreesToRadians(i)) * radius);
 			koordinaten.push(Math.sin(degreesToRadians(i)) * radius);
 
-			colors.push(1);
-			colors.push(0);
-			colors.push(1);
+			colors.push(0.101);
+			colors.push(0.78);
+			colors.push(0.835);
 			colors.push(1.0);
 
 			num_vertex += 1;
@@ -82,11 +78,11 @@ function drawPacman(radius, numberOfVertices, angleMouth)
 	// Configure viewport
 
 	gl.viewport(0,0,canvas.width,canvas.height);
-	gl.clearColor(1.0,1.0,1.0,1.0);
+	gl.clearColor(0, 0, 0, 1)
 
 	// Init shader program and bind it
 
-	var program = initShaders(gl, "vertex-shader", "fragment-shader");
+	program = initShaders(gl, "vertex-shader", "fragment-shader");
 	gl.useProgram(program);
 
 
@@ -112,16 +108,10 @@ function drawPacman(radius, numberOfVertices, angleMouth)
 	var vColor = gl.getAttribLocation(program, "vColor");
 	gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
 	gl.enableVertexAttribArray(vColor);
+}
 
-	// konstater Skalierungsfaktor, der als uniform auf jeden Vertex angewendet werden kann
-	//var scalarPosition = gl.getUniformLocation(program, "vScalar");
-	//gl.uniform1f(scalarPosition, 2.0);
-
-	// ÜBERGABE DER AKTUELLEN TRANSLATION
-	var transLoc = gl.getUniformLocation(program, "translation");
-	gl.uniform4fv(transLoc, new Float32Array([current_positionX, current_positionY, 0.0, 0.0]));
-
-
+function rotateAndTranslate()
+{
 	var cosB = Math.cos(degreesToRadians(current_rotation));
 	var sinB = Math.sin(degreesToRadians(current_rotation));
 
@@ -130,35 +120,87 @@ function drawPacman(radius, numberOfVertices, angleMouth)
 	gl.uniform1f(u_CosB, cosB);
 	gl.uniform1f(u_SinB, sinB);
 
-
+	var transLoc = gl.getUniformLocation(program, "translation");
+	gl.uniform4fv(transLoc, new Float32Array([current_positionX, current_positionY, 0.0, 0.0]));
 }
 
-function getSineForAnAngle(angleInDegrees) {
-	return Math.sin(angleInDegrees * Math.PI / 180);
+function getYPosition()
+{
+	var sinA = Math.sin(degreesToRadians(current_rotation));
+	return sinA * 0.05;
 }
 
-function getCosineForAnAngle(angleInDegrees) {
-	return Math.cos(angleInDegrees * Math.PI / 180);
+function getXPosition()
+{
+	if ( (current_rotation > 90) || (current_rotation < -90))
+	{
+		return -1* Math.sqrt((0.05*0.05) - (getYPosition() * getYPosition()));
+	}
+	else
+	{
+		return Math.sqrt((0.05*0.05) - (getYPosition() * getYPosition()));
+	}
 }
 
 function keyLogger(e)
 {
+	var border = 1 - radius;
+
 	switch(e.keyCode)
 	{
 		case 38:
-			current_positionX += 0.05; // bzw anders später
+
+			var newXPosition = current_positionX + getXPosition();
+			var newYPosition = current_positionY + getYPosition();
+
+			if ((newXPosition < border) && (newXPosition > - border) && (newYPosition < border) && (newYPosition > -border ))
+			{
+				current_positionX = newXPosition;
+				current_positionY = newYPosition;
+			}
+			else
+			{
+				alert("Bitte umdrehen");
+			}
 			break;
 		case 37:
-			console.log("Turn left");
-
+			if (current_rotation < 180)
+			{
+				current_rotation += 1;
+			}
+			else
+			{
+				current_rotation = -179;
+			}
 			break;
 		case 39:
-			console.log("Turn right");
+			if (current_rotation > -179)
+			{
+				current_rotation -= 1;
+			}
+			else
+			{
+				current_rotation = 180;
+			}
+			break;
+		case 40:
+			var newXPosition = current_positionX - getXPosition();
+			var newYPosition = current_positionY - getYPosition();
+
+			if ((newXPosition < border) && (newXPosition > - border) && (newYPosition < border) && (newYPosition > -border ))
+			{
+				current_positionX = newXPosition;
+				current_positionY = newYPosition;
+			}
+			else
+			{
+				alert("Bitte umdrehen");
+			}
 			break;
 	}
 }
-window.addEventListener("keydown", keyLogger);
 
+window.addEventListener("keydown", keyLogger);
 
 var render = function()
 {
