96546e895f9f7106e92d11a677930007e41dd31c
= <kruemel952@gmail.com>
2016-11-24 13:58:16 +0100

    Aus Zusammemarbeit vor Psycho Seminar
    Bewegung funktioniert grundlegend in 60 fps 😻🏂☺️😍🍟🏋️🏋️🏋️

--- ICG-04-3D/Cube/cube.js
+++ ICG-04-3D/Cube/cube.js
@@ -16,71 +16,78 @@ var viewMatrix;
 var projectionMatrixLoc;
 var projectionMatrix;
 
+var eye;
+var target;
+var up;
+
 window.onload = function init()
 {
 	// Get canvas and setup webGL
-	
+
 	canvas = document.getElementById("gl-canvas");
+	canvas.width = document.body.clientWidth;
+	canvas.height = document.body.clientHeight;
+
 	gl = WebGLUtils.setupWebGL(canvas);
 	if (!gl) { alert("WebGL isn't available"); }
 
 	// Specify position and color of the vertices
-	
+
 									 // Front
 	positions = new Float32Array([  -0.5, -0.5,  0.5,
 								     0.5, -0.5,  0.5,
 								     0.5,  0.5,  0.5,
-										
+
 									 0.5,  0.5,  0.5,
 									-0.5,  0.5,  0.5,
 									-0.5, -0.5,  0.5,
-										
+
 									 // Right
 									 0.5,  0.5,  0.5,
 									 0.5, -0.5,  0.5,
 									 0.5, -0.5, -0.5,
-										
+
 									 0.5, -0.5, -0.5,
 									 0.5,  0.5, -0.5,
 									 0.5,  0.5,  0.5,
-										
+
 									 // Back
 									-0.5, -0.5, -0.5,
 									 0.5, -0.5, -0.5,
 									 0.5,  0.5, -0.5,
-										
+
 									 0.5,  0.5, -0.5,
 									-0.5,  0.5, -0.5,
 									-0.5, -0.5, -0.5,
-										
+
 									 // Left
 									-0.5,  0.5,  0.5,
 									-0.5, -0.5,  0.5,
 									-0.5, -0.5, -0.5,
-										
+
 									-0.5, -0.5, -0.5,
 									-0.5,  0.5, -0.5,
 									-0.5,  0.5,  0.5,
-										
+
 									 // Bottom
 									-0.5, -0.5,  0.5,
 									 0.5, -0.5,  0.5,
 									 0.5, -0.5, -0.5,
-										
+
 									 0.5, -0.5, -0.5,
 									-0.5, -0.5, -0.5,
 									-0.5, -0.5,  0.5,
-										
+
 									 // Top
 									-0.5,  0.5,  0.5,
 									 0.5,  0.5,  0.5,
 									 0.5,  0.5, -0.5,
-										
+
 									 0.5,  0.5, -0.5,
 									-0.5,  0.5, -0.5,
 									-0.5,  0.5,  0.5
 								]);
-										
+
 									// Front
 	colors = new Float32Array([     0, 0, 1, 1,
 									0, 0, 1, 1,
@@ -88,15 +95,15 @@ window.onload = function init()
 									0, 0, 1, 1,
 									0, 0, 1, 1,
 									0, 0, 1, 1,
-									
+
 									// Right
-									0, 1, 0, 1, 
 									0, 1, 0, 1,
 									0, 1, 0, 1,
 									0, 1, 0, 1,
 									0, 1, 0, 1,
 									0, 1, 0, 1,
-									
+									0, 1, 0, 1,
+
 									// Back
 									1, 0, 0, 1,
 									1, 0, 0, 1,
@@ -104,25 +111,25 @@ window.onload = function init()
 									1, 0, 0, 1,
 									1, 0, 0, 1,
 									1, 0, 0, 1,
-									
+
 									// Left
-									1, 1, 0, 1, 
 									1, 1, 0, 1,
 									1, 1, 0, 1,
 									1, 1, 0, 1,
 									1, 1, 0, 1,
 									1, 1, 0, 1,
-									
+									1, 1, 0, 1,
+
 									// Bottom
-									1, 0, 1, 1, 
 									1, 0, 1, 1,
 									1, 0, 1, 1,
 									1, 0, 1, 1,
 									1, 0, 1, 1,
 									1, 0, 1, 1,
-									
+									1, 0, 1, 1,
+
 									// Top
-									0, 1, 1, 1, 
+									0, 1, 1, 1,
 									0, 1, 1, 1,
 									0, 1, 1, 1,
 									0, 1, 1, 1,
@@ -152,30 +159,30 @@ window.onload = function init()
 	gl.enableVertexAttribArray(vPosition);
 
 	// Load colors into the GPU and associate shader variables
-	
+
 	colorBuffer = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
 	gl.bufferData(gl.ARRAY_BUFFER, colors, gl.STATIC_DRAW);
-	
+
 	var vColor = gl.getAttribLocation(program, "vColor");
 	gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
 	gl.enableVertexAttribArray(vColor);
-	
+
 	// Set model matrix
-	
+
 	modelMatrix = new Float32Array([1, 0, 0, 0,
 									0, 1, 0, 0,
 									0, 0, 1, 0,
 									0, 0, 0, 1]);
-	
+
 	modelMatrixLoc = gl.getUniformLocation(program, "modelMatrix");
 	gl.uniformMatrix4fv(modelMatrixLoc, false, modelMatrix);
 
     // Set view matrix
 
-	var eye = vec3.fromValues(0.0, 0.0, 2.0);
-	var target = vec3.fromValues(0.0, 0.0, 0.0);
-	var up = vec3.fromValues(0.0, 1.0, 0.0);
+	eye = vec3.fromValues(0.0, 1.0, 2.0);
+	target = vec3.fromValues(0.0, 0.0, 0.0);
+	up = vec3.fromValues(0.0, 1.0, 0.0);
 
 	viewMatrix = mat4.create();
 	mat4.lookAt(viewMatrix, eye, target, up);
@@ -190,13 +197,115 @@ window.onload = function init()
 
 	projectionMatrixLoc = gl.getUniformLocation(program, "projectionMatrix");
 	gl.uniformMatrix4fv(projectionMatrixLoc, false, projectionMatrix);
-    
+
 	render();
 };
 
 function render()
 {
+	if (isDown[0])
+	{
+		moveForward();
+	}
+	if (isDown[1])
+	{
+		moveLeft();
+	}
+	if (isDown[2])
+	{
+		moveBackwards();
+	}
+	if (isDown[3])
+	{
+		moveRight();
+	}
+
+	mat4.lookAt(viewMatrix, eye, target, up);
+
+	gl.uniformMatrix4fv(viewMatrixLoc, false, viewMatrix);
+	gl.uniformMatrix4fv(projectionMatrixLoc, false, projectionMatrix);
+
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 	gl.drawArrays(gl.TRIANGLES, 0, positions.length/3);
+
 	requestAnimFrame(render);
+}
+
+// W, A, S, D
+var isDown = [false, false, false, false];
+
+function keyDown(e)
+{
+	switch (e.keyCode)
+	{
+		// W
+		case 87:
+		isDown[0] = true;
+		break;
+		// A
+		case 65:
+		isDown[1] = true;
+		break;
+		// S
+		case 83:
+		isDown[2] = true;
+		break;
+		// S
+		case 68:
+		isDown[3] = true;
+		break;
+	}
+}
+window.addEventListener("keydown", keyDown);
+
+function keyUp(e)
+{
+	switch (e.keyCode)
+	{
+		// W
+		case 87:
+		isDown[0] = false;
+		break;
+		// A
+		case 65:
+		isDown[1] = false;
+		break;
+		// S
+		case 83:
+		isDown[2] = false;
+		break;
+		// D
+		case 68:
+		isDown[3] = false;
+		break;
+	}
+}
+window.addEventListener("keyup", keyUp);
+
+
+// Hier wird die Bewegung ausgeführt
+
+// Kann modifiziert werden, um Bewegungsgeschwindigkeit zu ändern
+var speed = 0.03;
+function moveForward()
+{
+	eye[2] = eye[2] - speed;
+	target[2] = target[2] - speed;
+}
+function moveLeft()
+{
+	eye[0] = eye[0] - speed;
+	target[0] = target[0] - speed;
+}
+function moveBackwards()
+{
+
+	eye[2] = eye[2] + speed;
+	target[2] = target[2] + speed;
+}
+function moveRight()
+{
+
+	eye[0] = eye[0] + speed;
+	target[0] = target[0] + speed;
 }
